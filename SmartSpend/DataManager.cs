c02703cc26cafccfa196f40af7c450a4ca7c7a9d
OldPopeAndBrighter <szymon.mos@op.pl>
2025-05-15 18:28:05 +0200

    Added DataManager component

---
+++ SmartSpend/DataManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartSpend
+{
+    internal class DataManager 
+    {
+        public Expense GetExpenseById(int id) 
+        {
+            return new Expense(Categories.DailyLife,SubCategories.Rent,600);
+        }
+
+        public void AddNewExpense(Expense expense)
+        {
+            
+        }
+        public List<Expense> GetAllExpenses()
+        {
+            return new List<Expense>();
+        }
+      
+    }
+}
